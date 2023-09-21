40a1c84b23c082c4267ce3a12771f9fdf9a042b1
David <david.ndiba@student.moringaschool.com>
2023-09-21 15:04:04 +0300

    done with working with API'S lab

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -4,12 +4,21 @@ import json
 
 class GetPrograms:
 
-  def get_programs(self):
-    URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+    def get_programs(self):
+        url = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+        programs_list = []
 
-    response = requests.get(URL)
-    return response.content
+        response = requests.get(url)
+        if response.status_code == 200:
+            programs = json.loads(response.text)
+            for program in programs:
+                programs_list.append(program["agency"])
+        
+        return programs_list
 
+if __name__ == "__main__":
+    programs = GetPrograms()
+    programs_schools = programs.get_programs()
 
-programs = GetPrograms().get_programs()
-print(programs)
+    for school in set(programs_schools):
+        print(school)
